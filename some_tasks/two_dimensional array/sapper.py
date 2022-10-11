8c0cb9bd1248d0ce1a0d3abb895af1bf87e95dba
sl1tan <privet1796@yandex.ru>
2022-10-11 13:33:35 +0300

    add: 2arrays tasks

---
+++ some_tasks/two_dimensional array/sapper.py
@@ -0,0 +1,42 @@
+def output(arr):
+    for i in range(1, len(arr) - 1):
+        for j in range(1, len(arr[0]) - 1):
+            print("{:2}".format(arr[i][j]), end="")
+        print()
+
+def make_arr(n, m):
+    arr = [["0"] * m for i in range(n)]
+    return arr
+
+def sapper(arr):
+    for x in range(1, len(arr) - 1):
+        for y in range(1, len(arr[0]) - 1):
+            count = 0
+            if arr[x][y] != "*":
+                if arr[x][y + 1] == "*":
+                    count += 1
+                if arr[x][y - 1] == "*":
+                    count += 1
+                if arr[x + 1][y] == "*":
+                    count += 1
+                if arr[x - 1][y] == "*":
+                    count += 1
+                if arr[x + 1][y - 1] == "*":
+                    count += 1
+                if arr[x + 1][y + 1] == "*":
+                    count += 1
+                if arr[x - 1][y - 1] == "*":
+                    count += 1
+                if arr[x - 1][y + 1] == "*":
+                    count += 1
+                arr[x][y] = str(count)
+    return arr
+
+n, m, mines = [int(i) for i in input().split()]
+arr = make_arr(n + 2, m + 2)
+
+for i in range(mines):
+    x, y = [int(i) for i in input().split()]
+    arr[x][y] = "*"
+
+output(sapper(arr))
---
+++ some_tasks/two_dimensional array/spiral.py
@@ -0,0 +1,55 @@
+def output(arr):
+    for row in arr:
+        for elem in row:
+            print("%4d"%(elem), end="")
+        print()
+
+def make_arr(n, m):
+    arr = [[0] * m for i in range(n)]
+    return arr
+
+
+def spiral(arr):
+    i = 1
+    x, y = 0, 0
+    rb = m
+    db = n
+    lb = 0
+    ub = 0
+    while(i < n*m - 1):
+        while(y < rb):
+            arr[x][y] = i
+            y += 1 
+            i += 1
+        y -= 1
+        i -= 1
+        rb -= 1
+        while(x < db):
+            arr[x][y] = i
+            x += 1
+            i += 1
+        x -= 1
+        i -= 1
+        db -= 1
+        while(y >= lb):
+            arr[x][y] = i
+            y -= 1
+            i += 1
+        lb += 1
+        y += 1
+        i -= 1
+        while(x > ub):
+            arr[x][y] = i
+            x -= 1
+            i += 1
+        x += 1
+        i -= 1
+        ub += 1
+    return arr
+
+
+n, m = [int(i) for i in input().split()]
+arr = make_arr(n, m)
+arr = spiral(arr)
+
+output(arr)
